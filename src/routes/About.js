cb4ff8e66d4bf23a661fc15656153928d1b4080e
shivani.gupta-at-494107731180 <shivani.gupta@omnifi.in>
2024-08-18 14:00:52 +0530

    animation added

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Lottie from 'lottie-react'
+import animationData from "../assets/Animation - 1723969209966.json"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>About
+      <div style={{width:"50rem", height:"50rem", justifyContent:"center"}}>
+      <Lottie animationData={animationData}/>
+      </div>
+    </div>
   )
 }
 
